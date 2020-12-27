e0e6d92d6c72bd662c629c12548ff54879646d4b
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-27 23:13:17 +0000

    made navbar styles responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "@material-ui/icons";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const { pathname } = useLocation();
+  const links = [
+    { name: "Exercises", slug: "/" },
+    { name: "Create Exercises", slug: "/create" },
+    { name: "Create Users", slug: "/user" },
+  ];
 
   return (
     <nav className={styles.navbar}>
-      <Link to='/' className={styles.logo}>
-        Exercise Tracker
-      </Link>
+      <h1>
+        <Link to='/' className={styles.logo}>
+          Exercise Tracker
+        </Link>
+      </h1>
 
       <ul className={`${styles.navbarLinks} ${open ? styles.open : ""}`}>
-        <li className={styles.navbarLink}>
-          <Link to='/'>Exercises</Link>
-        </li>
-        <li className={styles.navbarLink}>
-          <Link to='/create'>Create Exercises Log</Link>
-        </li>
-        <li className={styles.navbarLink}>
-          <Link to='/user'>Create User</Link>
-        </li>
+        {links.map((link, index) => {
+          const { name, slug } = link;
+          const activeClass = pathname === slug ? styles.activeLink : "";
+          return (
+            <li className={`${styles.navbarLink} ${activeClass}`}>
+              <Link
+                to={slug}
+                onClick={() => {
+                  setOpen(prev => !prev);
+                }}
+              >
+                {name}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
       <button
         className={styles.navbarBtn}
